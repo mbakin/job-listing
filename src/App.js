2e4030323dec2762ddb2451b3dcc91428ccaa014
mbakin <mehmetberkakinn@outlook.com>
2021-08-22 00:33:05 +0300

    Added filter features and re-design layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,77 @@
 import React, { useState, useEffect } from "react";
 import JobBoards from "./components/JobBoards";
-import data from './assets/data.json'
-
+import data from "./assets/data.json";
 
 function App() {
-
-  const [jobs, setJobs]  = useState([]);
+  const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
-    setJobs(data)
-  }, [])
+    setJobs(data);
+  }, []);
 
+  const filterHandler = ({ role, level, languages, tools }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    if (tools) {
+      tags.push(...tools);
+    }
+
+    return filters.every(filter => tags.includes(filter))
+  };
+
+  const handleTagClick = (tag) => {
+    if(filters.includes(tag)) return;
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter(event => event !== passedFilter ))
+  }
+
+  const clearFilters = () => {
+    setFilters([])
+  }
+
+  const filteredJobs = jobs.filter(filterHandler);
 
   return (
     <div className="App">
-      <header className="justify-items-center items-center mb-12">
-        <img src="./images/bg-header-desktop.svg" alt="bg-header" />
+      <header className="bg-indigo-50  mb-12">
+        <img
+          className="w-full"
+          src="./images/bg-header-desktop.svg"
+          alt="bg-header"
+        />
       </header>
-      {
-        jobs.length === 0 ? (
-          <p>Jobs are fetching...</p>
-        ) : (
-          jobs.map(job => (
-            <JobBoards key={job.id} job={job}/>
-          ))
-        )
-      }
+        {filters.length > 0 && (
+          <div className={`flex bg-white w-4/5 mx-auto -my-20  mb-16 shadow-md p-6 rounded relative z-10`}>
+            {filters.map((filter) => (
+              <span
+                className="cursor-pointer rounded p-2 mr-4 font-bold bg-indigo-50 hover:bg-green-50 text-pink-500 lg:mb-0"
+                onClick={() => handleFilterClick(filter)}>
+                  {filter} ×
+              </span> 
+            ))}
+            <button onClick={clearFilters} className="font-bold text-gray-800 hover:text-gray-400 ml-auto">Clear</button>
+          </div> 
+
+      )}
+      {jobs.length === 0 ? (
+        <p>Jobs are fetching...</p>
+      ) : (
+        filteredJobs.map((job) => (
+          <JobBoards key={job.id} job={job} handleTagClick={handleTagClick} />
+        ))
+      )}
     </div>
   );
 }
--- src/components/JobBoards.js
+++ src/components/JobBoards.js
@@ -15,6 +15,7 @@ const JobBoards = ({
     languages,
     tools,
   },
+  handleTagClick
 }) => {
   const tags = [role, level];
 
@@ -27,25 +28,41 @@ const JobBoards = ({
   }
 
   return (
-    <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${featured && 'border-l-4 border-green-500 border-solid'} sm:flex-row`}>
+    <div
+      className={`flex flex-col bg-white shadow-md w-4/5 my-16 mx-auto p-6 rounded ${
+        featured && "border-l-4 border-green-500 border-solid"
+      } lg:flex-row`}
+    >
       <div>
-        <img className="w-20 h-20 -mt-16 mb-4 sm:mt-0 sm:h-24 sm:w-24 sm:my-0" src={logo} alt={company} />
+        <img
+          className="w-20 h-20 -mt-16 mb-4 lg:mt-0 lg:h-24 lg:w-24 lg:my-0"
+          src={logo}
+          alt={company}
+        />
       </div>
       <div className="flex flex-col justify-between ml-2">
         <h3 className="font-bold text-indigo-300">
           {company}
-          {isNew && (<span className="bg-pink-600 text-white  rounded-full py-1 px-2  m-2 uppercase text-sm">New</span>)}
-          {featured && (<span className="bg-green-500 text-white  rounded-full py-1 px-2 uppercase  text-sm">Featured</span>)}
+          {isNew && (
+            <span className="bg-pink-600 text-white  rounded-full py-1 px-2  m-2 uppercase text-sm">
+              New
+            </span>
+          )}
+          {featured && (
+            <span className="bg-green-500 text-white  rounded-full py-1 px-2 uppercase  text-sm">
+              Featured
+            </span>
+          )}
         </h3>
         <h2 className="font-bold text-xl my-2">{position}</h2>
         <p className="text-gray-500 py-3">
           {postedAt} · {contract} · {location}
         </p>
       </div>
-      <div className="flex flex-wrap items-center mt-4 mx-4  pt-4 border-t border-gray-200 border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0 sm:items-center">
+      <div className="flex flex-wrap items-center mt-4 mx-4  pt-4 border-t border-gray-200 border-solid lg:ml-auto lg:border-0 lg:pt-0 lg:mt-0 lg:items-center">
         {tags
           ? tags.map((tag) => (
-              <span className="bg-indigo-50 hover:bg-green-50 text-pink-500  rounded p-1 mb-4 mr-4 sm:mb-0">
+              <span onClick={() => handleTagClick(tag)} className="cursor-pointer bg-indigo-50 hover:bg-green-50 text-pink-500  rounded p-1 mb-4 mr-4 lg:mb-0">
                 {tag}
               </span>
             ))
